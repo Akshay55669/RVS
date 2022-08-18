70cbc3f6838c24536b9b2b95849c5e2ffbcc0519
Akshay55669 <akshaywahile97@gmail.com>
2022-08-18 17:39:12 +0530

    Backend

---
+++ RVS/RVS/Controllers/ReviewsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RVS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RVS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ReviewDbContext _context;
+        
+        public ReviewsController( ReviewDbContext context)
+        {
+            
+            _context = context;
+        }
+
+        [HttpPost("review")]
+        public ActionResult Create(Review review)
+        {
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+            return Ok("Success from creat method");
+        }
+    }
+}
---
+++ RVS/RVS/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RVS.Models
+{
+    public class Review
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(300)]
+        public String Email { get; set; }
+        [MaxLength(300)]
+        public string CompanyName { get; set; }
+        [MaxLength(300)]
+        public string Designation { get; set; }
+        [MaxLength(1000)]
+        public string Reviews { get; set; }
+    }
+}
